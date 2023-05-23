c6b0f997ce9f0b7748faabaa137fe33e8115afb2
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-23 18:03:05 +0200

    build: add junit for unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,19 @@
 plugins {
+    id("java")
     alias(libs.plugins.taskTree)
 }
 
+group = "io.github.rustfields"
+
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
