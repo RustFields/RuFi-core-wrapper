bc323ca11335c3f84feb1510844e8aa87c5aec2f
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-23 17:40:28 +0200

    build: add Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.taskTree)
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "rufi-core-wrapper"
+val destination = File(".git/hooks/commit-msg")
+
+plugins {
+  id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.7"
+}
+
+gitHooks {
+  commitMsg {
+    conventionalCommits()
+  }
+  createHooks(true)
+}
