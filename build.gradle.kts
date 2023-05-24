58c31ec2487279a3999d4afba48a44582a839bd6
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-24 12:32:20 +0200

    ci: configure Semantic Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.spotBugs)
     checkstyle
     pmd
+    alias(libs.plugins.publishCentral)
 }
 
 group = "io.github.rustfields"
@@ -19,4 +20,48 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishOnCentral {
+    projectUrl.set("https://github.com/RustFields/${project.name}")
+    scmConnection.set("git:git@github.com:RustFields/${project.name}")
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            pom {
+                developers {
+                    developer {
+                        name.set("Filippo Vissani")
+                        email.set("filippo.vissani@studio.unibo.it")
+                        url.set("https://filippovissani.github.io/")
+                    }
+                    developer {
+                        name.set("Leonardo Micelli")
+                        email.set("leonardo.micelli@studio.unibo.it")
+                        url.set("https://lm98.github.io/")
+                    }
+                    developer {
+                        name.set("Paolo Penazzi")
+                        email.set("paolo.penazzi@studio.unibo.it")
+                        url.set("https://paolopenazzi.github.io/")
+                    }
+                    developer {
+                        name.set("Angela Cortecchia")
+                        email.set("angela.cortecchia@studio.unibo.it")
+                        url.set("https://angelacorte.github.io/")
+                    }
+                }
+            }
+        }
+    }
+}
+
+if(System.getenv("CI") == true.toString()){
+    signing {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
 }
