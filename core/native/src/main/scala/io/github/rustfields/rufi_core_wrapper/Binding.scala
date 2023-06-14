e65e9924d57ce4bd7ea353ed6e7d7829426d4cdd
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-06-14 16:35:47 +0200

    chore: improve example

--- core/native/src/main/scala/io/github/rustfields/rufi_core_wrapper/Binding.scala
+++ core/native/src/main/scala/io/github/rustfields/rufi_core_wrapper/Binding.scala
@@ -2,7 +2,7 @@ package io.github.rustfields.rufi_core_wrapper
 
 import io.github.rustfields.rufi_core_wrapper.TypeBinding.*
 import scala.scalanative.unsafe
-import scala.scalanative.unsafe.extern
+import scala.scalanative.unsafe.{CFuncPtr1, extern}
 
 object TypeBinding:
   type CInt = unsafe.CInt
@@ -10,11 +10,15 @@ object TypeBinding:
 
 @extern
 object Native:
-  def divide(a: CInt, b: CInt): CInt = extern
+  def divide(a: CInt, b: CInt): CInt =
+    extern
 
-  def generic_operation(x: CInt, f: CFunIntToInt): CInt = extern
+  def generic_operation(x: CInt, f: CFunIntToInt): CInt =
+    extern
 
 object Binding:
-  def divide(a: CInt, b: CInt): CInt = Native.divide(a, b)
+  def divide(a: CInt, b: CInt): CInt =
+    Native.divide(a, b)
 
-  def genericOperation(x: CInt, f: CFunIntToInt): CInt = Native.generic_operation(x, f)
+  def genericOperation(x: CInt, f: CFunIntToInt): CInt =
+    Native.generic_operation(x, f)
