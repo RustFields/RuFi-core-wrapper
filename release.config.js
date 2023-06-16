a22358f6c85d83effeb3c4d18f17fcc4c868b52f
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-06-16 16:01:36 +0200

    ci: add prepareCommands in Semantic Release configuration

--- release.config.js
+++ release.config.js
@@ -1,18 +1,22 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
+const prepareCommands = `
+sed -i 's/version := .*/version := "${nextRelease.version}"/g' build.sbt || exit 1
+sed -i 's/version.*/version = "${nextRelease.version}"/g' native/Cargo.toml || exit 2
+git add -A || exit 3
+git commit -m "chore: [skip ci] update version in build.sbt and in Cargo.toml" || exit 4
+git push --force origin || exit 5`
+
 const publishCommands = `
-sed -i 's/version := .*/version := "\${nextRelease.version}"/g' build.sbt || exit 1
-git add -A || exit 2
-git commit -m "chore: [skip ci] update version in build.sbt" || exit 3
-git push --force origin || exit 4
-git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md  || exit 5
-git push --force origin \${nextRelease.version} || exit 6
-sbt cargoBuild || exit 7
-sbt publishLocal || exit 8
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md  || exit 1
+git push --force origin \${nextRelease.version} || exit 2
+sbt cargoBuild || exit 3
+sbt publishLocal || exit 4
 `
 const releaseBranches = ["main"]
 config.branches = releaseBranches
 config.plugins.push(
     ["@semantic-release/exec", {
+        "prepareCmd": prepareCommands,
         "publishCmd": publishCommands,
     }],
     ["@semantic-release/github", {
