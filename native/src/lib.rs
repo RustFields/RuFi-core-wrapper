c87c6fe91eaa08411504a95a946c6c9cf6078b42
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-06-20 11:45:49 +0200

    ci: add support for multiplatform
    * ci: test multiplatform build workflow
    
    * chore: remove compilation warning
    
    * ci: fix build-native-library job
    
    * ci: configure ci to publish native libraries
    
    * ci: improve workflow efficiency
    
    * ci: remove unused job dependencies
    
    * ci: improve workflow
    
    * ci: fix conditions in workflow
    
    * ci: fix path in workflow
    
    * ci: fix permission problem in MacOS
    
    * ci: improve workflow
    
    * chore: add dynamic library to .gitignore

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -9,7 +9,7 @@ pub mod operations {
     #[no_mangle]
     pub extern "C" fn generic_operation(
         x: i32,
-        f: fn(i32) -> i32
+        f: extern fn(i32) -> i32
     ) -> i32 {
         f(x)
     }
